151c31b3d2909a2a0d7c477185f4854fc1ea3213
Lahfir <nmhlahfir2@gmail.com>
2025-05-31 07:32:18 +0530

    Initial Commit

---
+++ CursorDisk/CursorDiskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursorDiskApp.swift
+//  CursorDisk
+//
+//  Created by Lahfir on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct CursorDiskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursorDiskTests/CursorDiskTests.swift
@@ -0,0 +1,17 @@
+//
+//  CursorDiskTests.swift
+//  CursorDiskTests
+//
+//  Created by Lahfir on 5/31/25.
+//
+
+import Testing
+@testable import CursorDisk
+
+struct CursorDiskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
